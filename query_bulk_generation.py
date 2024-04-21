a6a6e44b12734f820bf07a6bcc315c08801161f1
s28zabed <zu2377@gmail.com>
2024-04-21 12:18:14 +0200

    f

--- query_bulk_generation.py
+++ query_bulk_generation.py
@@ -1,156 +1,112 @@
 import json
+import os
+import pprint
 
+#hi bro
 def extract_question_answer_pairs(data):
-    questions = []
-    answers = []
-    qa_pairs=[]
-
-    for product_key(B00836Y6B2), product_data in data.items():
-        for conv_type(Opos1B_Opos1B2_only_agreement), conv_type_data in product_data.items():
-            
-            for conv_key(Opos1B_Opos1B2_1), conv_data in conv_type_data.items():
-                    if conv_type == 'Qpos1A_Apos1A':
-                        for pair_key("Opos1B"), pair_data in conv_data.items():
-                            
-
-                                if 'Qpos1A' in pair_key:
-                                    question = pair_data[pair_key][Question]
-                                    key_question =  pair_data[pair_key][Labels][Key]
-                                elif 'Apos1A' in pair_key:
-                                    answer = pair_data[pair_key][Answer]
-                                    key_answer =  pair_data[pair_key][Labels][Key]
-
-                               # qa_pairs.append((key_question, key_answer, question, answer))
-                               qa_pairs.append((
-                                key_question,
-                                key_answer,
-                                question,
-                                answer
-                            ))
-
-                    else:
-                        counter = 1
-                        for pair_key("Opos1B"), pair_data in conv_data.items():
-                            
-
-                                if counter==1:
-                                    question = pair_data[pair_key][Opinion]
-                                    key_question =  pair_data[pair_key][Labels][Key]
-                                    counter++;
-                                elif counter ==2:
-                                    answer = pair_data[pair_key][Opinion]
-                                    key_answer =  pair_data[pair_key][Labels][Key]
-
-                                qa_pairs.append((
-                                key_question,
-                                key_answer,
-                                question,
-                                answer
-                            ))
-
-
-    return qa_pairs
-
- "B00836Y6B2": {
-        "Opos1B_Opos1B2_only_agreement": {
-            "Opos1B_Opos1B2_1": {
-                "Opos1B": {
-                    "Opinion": "I was wondering if you have this phone B00836Y6B2, it might be a good choice because as far as I know about its price, Good product, great price.",
-                        "Labels": {
-                        "Key": "B00836Y6B2_AFGB723G8ECAX_0_0",
-                            "Aspect": "price",
-                                "Polarity": "positive"
-                    }
-                },
-                "Opos1B2": {
-                    "Opinion": "Yes, That's so true. This phone is also a good choice",
-                        "Labels": {
-                        "Key": "B00836Y6B2_AFGB723G8ECAX_0_0",
-                            "Aspect": "price",
-                                "Polarity": "positive"
-                    }
-                }
-            },
-   "B00836Y6B2": {
-        "Qpos1A_Apos1A": {
-            "Qpos1A_Apos1A_1": {
-                "Qpos1A": {
-                    "Question": "In your honest opinion, how is its price?",
-                        "Labels": {
-                        "Key": "B00836Y6B2_AFGB723G8ECAX_0_0",
-                            "Aspect": "price",
-                                "Polarity": "positive"
-                    }
-                },
-                "Apos1A": {
-                    "Answer": "Good product, great price.",
-                        "Labels": {
-                        "Key": "B00836Y6B2_AFGB723G8ECAX_0_0",
-                            "Aspect": "price",
-                                "Polarity": "positive"
-                    }
-                }
-            },
-
-
-# Load the JSON file
-with open("/content/drive/MyDrive/RAG/dataset.json", "r") as f:
-    json_data = json.load(f)
-
-# Extract question-answer pairs
-questions, answers = extract_question_answer_pairs(json_data)
-
-# Print the question-answer pairs
-for q, a in zip(questions, answers):
-    print("Question:", q)
-    print("Answer:", a)
-    print()
-
-def extract_qa_pairs(data, outer_key=None):
+    
     qa_pairs = []
 
-    for key, value in data.items():
-        if key == "Qpos1A_Apos1A":
-            for pair in value.values():
-                question = pair["Qpos1A"]["Question"]
-                answer = pair["Apos1A"]["Answer"]
-                qa_pairs.append((outer_key, question, answer))
+    for product_key, product_data in data.items():
+        for conv_type, conv_type_data in product_data.items():
+            for conv_key, conv_data in conv_type_data.items():
+                if conv_type == 'Qpos1A_Apos1A':
+                    for pair_key, pair_data in conv_data.items():
+                        pprint.pprint(pair_key)
+                        pprint.pprint(pair_data)
+                        if 'Qpos1A' in pair_key:
+                            question = pair_data['Question']
+                            key_question = pair_data['Labels']['Key']
+                        elif 'Apos1A' in pair_key:
+                            answer = pair_data['Answer']
+                            key_answer = pair_data['Labels']['Key']
 
-        elif key.startswith("Oneg1A_Opos"):
-            for pair in value.values():
-                question = pair["Oneg1A"]["Opinion"]
-                answer = pair["Opos1A"]["Opinion"]
-                qa_pairs.append((outer_key, question, answer))
+                    qa_pairs.append((
+                            key_question,
+                            key_answer,
+                            question,
+                            answer
+                        ))
+                else:
+                    counter = 1
+                    for pair_key, pair_data in conv_data.items():
+                        pprint.pprint(pair_key)
+                        pprint.pprint(pair_data)
+                        if counter == 1:
+                            question = pair_data['Opinion']
+                            key_question = pair_data['Labels']['Key']
+                            counter += 1
+                        elif counter == 2:
+                            answer = pair_data['Opinion']
+                            key_answer = pair_data['Labels']['Key']
 
-        elif isinstance(value, dict):
-            qa_pairs.extend(extract_qa_pairs(value, key))
+                    qa_pairs.append((
+                            key_question,
+                            key_answer,
+                            question,
+                            answer
+                        ))
 
     return qa_pairs
 
-# Example usage
-data = {
-    "B00J9XQRFG": {
-        "Qpos1A_Apos1A": {},
-        "Oneg1A_Opos1A": {},
-        "Oneg1A_Opos1B_retrieved": {},
-        # ... other keys
-    },
-    "B00836Y6B2": {
-        "Qpos1A_Apos1A": {
-            # ... question-answer pairs
-        },
-        "Oneg1A_Opos1A": {
-            # ... opinion-opinion pairs
-        },
-        # ... other keys
-    },
-    # ... other outer keys
-}
+def load_json(file_path):
+    """Load data from a JSON file."""
+    if not os.path.exists(file_path):
+        print(f"File '{file_path}' not found.")
+        return []
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    return data
 
-qa_pairs = extract_qa_pairs(data)
 
-for outer_key, question, answer in qa_pairs:
-    print(f"Outer Key: {outer_key}")
-    print(f"Question: {question}")
-    print(f"Answer: {answer}")
-    print()
+   # File path to your JSON file
+file_path = "/Users/zainabedin/Desktop/RAG/dataset.json"
+
+    # Load data from JSON file
+data = load_json(file_path)
+print("zainnnn")
+#pprint.pprint(data)
+    # Create a Hugging Face dataset
+qa_pairs = extract_question_answer_pairs(data)
+print (qa_pairs)
+
+ # "B00836Y6B2": {
+ #        "Opos1B_Opos1B2_only_agreement": {
+ #            "Opos1B_Opos1B2_1": {
+ #                "Opos1B": {
+ #                    "Opinion": "I was wondering if you have this phone B00836Y6B2, it might be a good choice because as far as I know about its price, Good product, great price.",
+ #                        "Labels": {
+ #                        "Key": "B00836Y6B2_AFGB723G8ECAX_0_0",
+ #                            "Aspect": "price",
+ #                                "Polarity": "positive"
+ #                    }
+ #                },
+ #                "Opos1B2": {
+ #                    "Opinion": "Yes, That's so true. This phone is also a good choice",
+ #                        "Labels": {
+ #                        "Key": "B00836Y6B2_AFGB723G8ECAX_0_0",
+ #                            "Aspect": "price",
+ #                                "Polarity": "positive"
+ #                    }
+ #                }
+ #            },
+ #   "B00836Y6B2": {
+ #        "Qpos1A_Apos1A": {
+ #            "Qpos1A_Apos1A_1": {
+ #                "Qpos1A": {
+ #                    "Question": "In your honest opinion, how is its price?",
+ #                        "Labels": {
+ #                        "Key": "B00836Y6B2_AFGB723G8ECAX_0_0",
+ #                            "Aspect": "price",
+ #                                "Polarity": "positive"
+ #                    }
+ #                },
+ #                "Apos1A": {
+ #                    "Answer": "Good product, great price.",
+ #                        "Labels": {
+ #                        "Key": "B00836Y6B2_AFGB723G8ECAX_0_0",
+ #                            "Aspect": "price",
+ #                                "Polarity": "positive"
+ #                    }
+ #                }
+ #            },
