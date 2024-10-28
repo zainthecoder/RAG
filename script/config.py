89022976a6e47082aec130bc4b462ae7bf691f59
s28zabed <zu2377@gmail.com>
2024-10-29 00:59:45 +0100

    commit 1

--- script/config.py
+++ script/config.py
@@ -57,19 +57,12 @@ def get_embedding_model():
 
 def get_reader_model():
     if 'reader_model' not in model_singleton:
-        READER_MODEL_NAME = "meta-llama/Meta-Llama-3-8B-Instruct"
-        bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.bfloat16,
-        )
+        READER_MODEL_NAME = "meta-llama/Meta-Llama-3-70B-Instruct"
         
         model_singleton['reader_model'] = AutoModelForCausalLM.from_pretrained(
             READER_MODEL_NAME,
             token=access_token,
             device_map={"": 0},
-            quantization_config=bnb_config,
             torch_dtype="auto",
             trust_remote_code=True,
         )
@@ -77,6 +70,6 @@ def get_reader_model():
 
 def get_tokenizer():
     if 'tokenizer' not in model_singleton:
-        READER_MODEL_NAME = "meta-llama/Llama-3.1-8B-Instruct"
+        READER_MODEL_NAME = "meta-llama/Meta-Llama-3-70B-Instruct"
         model_singleton['tokenizer'] = AutoTokenizer.from_pretrained(READER_MODEL_NAME, token=access_token,device_map="auto")
     return model_singleton['tokenizer']
